a5e011bdee62bd21030fb52c59ea17bd9f5a1352
dilnoza nabiyeva <dilnozanh09@gmail.com>
2023-02-05 10:21:22 -0500

    8

--- src/test/java/techproed/notes.java
+++ src/test/java/techproed/notes.java
@@ -24,3 +24,16 @@ Data provider has 2 parameters : name(to override the method name) and parallel(
 How did you use it?
 I used it when I was using testNG framework. In fact, I used data provider to get some of the test data from the excel sheet. We store the login credentials in excel and used data provider to get the data from there. The benefit was I can get multiple data without any loop.
  */
+
+/*
+JUnit: A testing framework. Has less annotation compare to TestNG
+TestNG: An advanced testing framework. Advanced version of JUnit framework
+Page Object Model: Popular Framework Design. We use page  object model for
+creating reusable page objects and methods.
+Maven: Project built tool. With maven, we can easily add other dependencies
+(junit,testng,selenium, faker, poi,…) to our project. Instead of importing the jar files,
+we just add the dependency in the pom.xml file. Then maven automatically imports jar pies for us.
+Developers may also use maven for their project built.
+Selenium: Selenium is used to test web based application.
+We can also do mobile testing with 3rd party tool such as appium.
+ */
